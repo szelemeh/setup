0cddcc50b6ca91e218b79459686e6b416ce60bc8
szelemeh <subzarro6@gmail.com>
2021-12-14 11:02:57 +0100

    Add VSCode settings

--- setup.sh
+++ setup.sh
@@ -1,6 +1,17 @@
+# Manual
+# - Download and install VSCode
+
 # Install brew packages
 brew bundle
 
 # Write zsh terminal config
 cp .zshrc ~/.zshrc
 cp .p10k.zsh ~/.p10k.zsh
+
+# Import VSCode extensions
+cat vscode-extensions.txt | while read ext 
+do
+  code --install-extension $ext > /dev/null
+done
+
+# Import VSCode settings
